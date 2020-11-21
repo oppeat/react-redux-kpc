77b257ea84e9e611f4133ff107647d6bccff25be
Peat <pasachon@mm.co.th>
2020-11-21 20:08:31 +0700

    Save records to localstorage

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ import { Provider } from 'react-redux'
 import 'semantic-ui-css/semantic.min.css'
 import { createStore } from 'redux'
 import rootReducer from './reducers'
+import { loadState, saveState } from './localStorage'
 
-const store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const persistStore = loadState()
+const store = createStore(rootReducer,persistStore,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 ReactDOM.render(
   <Provider store={store}>
@@ -21,3 +27,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default store
